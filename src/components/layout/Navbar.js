f34013e317621ec36328a2e85477e43b1c571b04
You Song Hou <yousonghou@yahoo.com>
2020-07-20 21:19:58 -0400

    show different navbar for guest and user

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import { Container, AppBar, Drawer, Toolbar, IconButton, List, ListItem, ListItemText, Typography, Button } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 
+import { GlobalContext } from '../../context/GlobalState';
 import { primaryColor } from '../../config/color';
 import Logo from '../../assets/logo.svg';
 
@@ -29,9 +30,41 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const Navbar = () => {
+    const { user } = useContext(GlobalContext);
     const classes = useStyles();
 
     const [open, setOpen] = useState(false);
+
+    const guestNavbar = (
+        <List className={classes.sideDrawer} component="nav">
+            <ListItem>
+                <img className={classes.logo} src={Logo} alt="Logo" />
+            </ListItem>
+            <ListItem button component={RouterLink} to="/">
+                <ListItemText primary="Home" />
+            </ListItem>
+            <ListItem button component={RouterLink} to="/login">
+                <ListItemText primary="Login" />
+            </ListItem>
+            <ListItem button component={RouterLink} to="/register">
+                <ListItemText primary="Register" />
+            </ListItem>
+        </List>
+    );
+
+    const userNavbar = (
+        <List className={classes.sideDrawer} component="nav">
+            <ListItem>
+                <img className={classes.logo} src={Logo} alt="Logo" />
+            </ListItem>
+            <ListItem button component={RouterLink} to="/">
+                <ListItemText primary="Home" />
+            </ListItem>
+            <ListItem button component={RouterLink} to="/profile">
+                <ListItemText primary="Profile" />
+            </ListItem>
+        </List>
+    );
 
     return(
         <div className={classes.root}>
@@ -50,20 +83,7 @@ const Navbar = () => {
                 </Container>
 
                 <Drawer anchor="right" open={open} onClick={() => setOpen(false)} onClose={() => setOpen(false)} onKeyDown={() => setOpen(false)}>
-                    <List className={classes.sideDrawer} component="nav">
-                        <ListItem>
-                            <img className={classes.logo} src={Logo} alt="Logo" />
-                        </ListItem>
-                        <ListItem button component={RouterLink} to="/">
-                            <ListItemText primary="Home" />
-                        </ListItem>
-                        <ListItem button component={RouterLink} to="/login">
-                            <ListItemText primary="Login" />
-                        </ListItem>
-                        <ListItem button component={RouterLink} to="/register">
-                            <ListItemText primary="Register" />
-                        </ListItem>
-                    </List>
+                    {user.email ? userNavbar : guestNavbar }
                 </Drawer>
             </AppBar>
         </div>
